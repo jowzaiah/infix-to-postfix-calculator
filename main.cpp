b14e05b314670ad0b9f01feada65cf074f02f85b
Goodness Agboola <agboolagoodness@gmail.com>
2018-12-27 00:40:14 -0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "calculator.h"  //include the header file
+
+int main()
+{
+	calculator mycal;  //create a calculator object
+
+	mycal.start();     //call the start function
+
+  return 0;
+}
